7025d0a13f99014901b9b80015ba0fd17e80646c
Mohammed Sahil Khan <mohammedsahilkhan.msk@gmail.com>
2024-11-18 20:05:36 +0530

    cardtype

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
-import StartupCard, { StartupCardType } from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { auth } from "@/auth";
@@ -49,7 +49,7 @@ export default async function Home({
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post:StartupCardType,index :number)=>(
+            posts.map((post:StartupTypeCard,index :number)=>(
               <StartupCard key={post?._id} post={post}/>
             ))
             ):(
--- components/UserStartups.tsx
+++ components/UserStartups.tsx
@@ -1,14 +1,14 @@
 import { client } from '@/sanity/lib/client'
 import { STARTUPS_BY_AUTHOR_QUERY } from '@/sanity/lib/queries'
 import React from 'react'
-import StartupCard, { StartupCardType } from './StartupCard'
+import StartupCard, { StartupTypeCard } from './StartupCard'
 
 const UserStartups = async({id}:{id:string}) => {
     const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY,{id})
 
   return (
     <>
-    {startups.length >0 ? startups.map((startup:StartupCardType)=>(
+    {startups.length >0 ? startups.map((startup:StartupTypeCard)=>(
         <StartupCard key={startup._id} post={startup}/>)
 
     ):(
